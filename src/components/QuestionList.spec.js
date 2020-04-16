c0417a544c5d966d8b1e5678c1ea3446e1eff2ae
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-16 10:01:51 -0400

    add before/after each/all globals

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,20 @@
 describe('The question list', () => {
+  beforeEach(() => {
+    console.log('before each');
+  });
+  beforeAll(() => {
+    console.log('before all');
+  });
+  afterEach(() => {
+    console.log('after each');
+  });
+  afterAll(() => {
+    console.log('after all');
+  });
   it('should display a list of items', () => {
+    expect(2+2).toEqual(4);
+  });
+  it('should display a list of items2', () => {
     expect(2+2).toEqual(4);
   });
 });
