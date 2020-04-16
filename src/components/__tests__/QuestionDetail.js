96f8cc1034b91178c63a7412a1fec3ad91a81798
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-04-16 13:06:17 -0400

    test mapstatetoprops

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,4 +1,24 @@
+import { mapStateToProps } from '../QuestionDetail';
+
 describe('The Question Detail Component', () => {
+  describe('The container element', () => {
+    describe('mapStateToProps', () => {
+      it('should map the state to props correctly', () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: 'the answer',
+        };
+        const appState = {
+          questions: [sampleQuestion]
+        };
+        const ownProps = {
+          question_id: 42
+        };
+        const componentState = mapStateToProps(appState, ownProps);
+        expect(componentState).toEqual(sampleQuestion);
+      });
+    });
+  });
   it('should not regress', () => {
     
   });
